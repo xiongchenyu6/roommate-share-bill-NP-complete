accd98a7db9f362bea03ea8b8ec081443d84ea66
xiongchenyu6 <xiongchenyu6@gmail.com>
2018-02-12 08:27:34 +0800

    Remove dead code

--- tests/tests.hs
+++ tests/tests.hs
@@ -10,17 +10,14 @@ main = quickCheck $
   property $ prop_SumOfAllSplitEqualToSumOfALLPositive
 
 
-data Pair a b = Pair a b deriving (Eq, Show)
 newtype SimpleTransactions = SimpleTransactions [(Name,Balance)] deriving (Eq,Show)
 
-pairGen :: (Arbitrary a, Arbitrary b) => Gen (a,b)
-pairGen = do
-  a <- arbitrary
-  b <- arbitrary
-  return (a,b)
-
-pairGenTransaction :: Gen (Name,Balance)
-pairGenTransaction = pairGen
+instance Arbitrary SimpleTransactions where
+  arbitrary = sized $ \s -> do
+                 n <- choose (0,s `min` 19)
+                 xs <- vectorOf n arbitrary
+                 return (SimpleTransactions xs)
+  shrink (SimpleTransactions xs) = map SimpleTransactions (shrink xs)
 
 prop_SumOfAllSplitEqualToSumOfALLPositive :: SimpleTransactions -> Bool
 prop_SumOfAllSplitEqualToSumOfALLPositive (SimpleTransactions l) = (totalTransfor - totalTransforDemand) < 100
@@ -32,10 +29,3 @@ prop_SumOfAllSplitEqualToSumOfALLPositive (SimpleTransactions l) = (totalTransfo
     totalTransfor = sum $ trd <$> (process bal)
     trd :: (a,a,b) -> b
     trd (_,_,x) = x
-
-instance Arbitrary SimpleTransactions where
-  arbitrary = sized $ \s -> do
-                 n <- choose (0,s `min` 19)
-                 xs <- vectorOf n arbitrary
-                 return (SimpleTransactions xs)
-  shrink (SimpleTransactions xs) = map SimpleTransactions (shrink xs)
